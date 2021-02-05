90e71b6e22ba113d70aa010ff504d04059c95b0b
sam-skinner <samuelskinner1998@gmail.com>
2021-02-05 15:28:55 -0500

    Cleaning up

--- php-code-test/index.php
+++ php-code-test/index.php
@@ -5,74 +5,74 @@
 <body>
 
 <?php
+//connect to database
+//connection parameters are in another file that I didn't upload
 include "db_connection.php";
 $conn = OpenCon();
 
-function printTable($result){
+//helper function that prints data from a select statement
+function printTable($result,$val){
   if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-      echo "id: " . $row["orderid"]. ", comment: " . $row["comments"]. "<br>";
+      echo "id: " . $row["orderid"] . " " . $val . ": " . $row[$val] . "<br>";
     }
   } else {
     echo "0 results";
   }
 }
 
-// $sql="SELECT * FROM sweetwater_test WHERE comments LIKE '%candy%';";
-// $result = mysqli_query($conn, $sql);
-// echo "Candy:<br>";
-// printTable($result);
-// echo "<br>";
+//CANDY
+$sql="SELECT * FROM sweetwater_test WHERE comments LIKE '%candy%';";
+$result = mysqli_query($conn, $sql);
+echo "<strong>Candy:</strong><br>";
+printTable($result, "comments");
+echo "<br>";
 
-// $sql="SELECT * FROM sweetwater_test WHERE comments LIKE '%call%';";
-// $result = mysqli_query($conn, $sql);
-// echo "Call:<br>";
-// printTable($result);
-// echo "<br>";
+//CALL
+$sql="SELECT * FROM sweetwater_test WHERE comments LIKE '%call%';";
+$result = mysqli_query($conn, $sql);
+echo "<strong>Call:</strong><br>";
+printTable($result, "comments");
+echo "<br>";
 
-// $sql="SELECT * FROM sweetwater_test WHERE comments LIKE '%refer%';";
-// $result = mysqli_query($conn, $sql);
-// echo "Refer:<br>";
-// printTable($result);
-// echo "<br>";
+//REFER
+$sql="SELECT * FROM sweetwater_test WHERE comments LIKE '%refer%';";
+$result = mysqli_query($conn, $sql);
+echo "<strong>Refer:</strong><br>";
+printTable($result, "comments");
+echo "<br>";
 
-// $sql="SELECT * FROM sweetwater_test WHERE comments LIKE '%signature%';";
-// $result = mysqli_query($conn, $sql);
-// echo "Signature:<br>";
-// printTable($result);
-// echo "<br>";
+//SIGNATURE
+$sql="SELECT * FROM sweetwater_test WHERE comments LIKE '%signature%';";
+$result = mysqli_query($conn, $sql);
+echo "<strong>Signature:</strong><br>";
+printTable($result, "comments");
+echo "<br>";
 
-// $sql="SELECT * FROM sweetwater_test 
-//       WHERE comments NOT LIKE '%candy%' 
-//       AND comments NOT LIKE '%call%' 
-//       AND comments NOT LIKE '%refer%' 
-//       AND comments NOT LIKE '%signature%';";
-// $result = mysqli_query($conn, $sql);
-// echo "Other:<br>";
-// printTable($result);
-// echo "<br>";
+//OTHERS
+$sql="SELECT * FROM sweetwater_test 
+      WHERE comments NOT LIKE '%candy%' 
+      AND comments NOT LIKE '%call%' 
+      AND comments NOT LIKE '%refer%' 
+      AND comments NOT LIKE '%signature%';";
+$result = mysqli_query($conn, $sql);
+echo "<strong>Other:</strong><br>";
+printTable($result, "comments");
+echo "<br>";
 
-// $sql="UPDATE sweetwater_test
-//       SET shipdate_expected = CONVERT(datetime, RIGHT(comments,8))
-//       WHERE comments in (SELECT comments FROM sweetwater_test WHERE comments LIKE '%Expected Ship Date%')";
-
+//DATE PARSE
+//finds comments with an Expected Shipping Date, converts the given date into a form that can be put into the sql column
 $sql="UPDATE sweetwater_test
-      SET shipdate_expected = CONVERT(DATE_FORMAT(SUBSTRING(comments, LOCATE('Expected Ship Date',comments)+20, 8),'%d/%y/%m'),datetime)
+      SET shipdate_expected = 
+        CONVERT(DATE_FORMAT(SUBSTRING(comments, LOCATE('Expected Ship Date',comments)+20, 8),'%d/%y/%m'), datetime)
       WHERE comments LIKE '%Expected Ship Date%'";
 $result = mysqli_query($conn, $sql);
 
-$sql="SELECT shipdate_expected FROM sweetwater_test WHERE shipdate_expected NOT LIKE '1000-01-01 00:00:00'";
+$sql="SELECT * FROM sweetwater_test WHERE shipdate_expected NOT LIKE '1000-01-01 00:00:00'";
 $result = mysqli_query($conn, $sql);
-echo "Date Parse:<br>";
-if (mysqli_num_rows($result) > 0) {
-  while($row = mysqli_fetch_assoc($result)) {
-    echo $row["shipdate_expected"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
+echo "<strong>Date Parse:</strong><br>";
+printTable($result, "shipdate_expected");
 echo "<br>";
-
 
 CloseCon($conn);
 ?>
--- php-code-test/solutions.md
+++ php-code-test/solutions.md
@@ -1,3 +1,9 @@
 First, I encountered a problem executing the giving sql file. 
 It turns out the datetime format has a minimum value of 1000-01-01, so I changed all the dates to be able to import the table.
 
+The first task was rather simple. I just queried based on the comments containing a keyword. I went back to make the data look a bit better.
+
+For the second task, I was stuck for some time on datetime conversion since SQL assumed that the year came first rather than month. My string indexing also is super messy. I know there has to be a better way, but I think I've spent enough time on this already.
+
+Cleaned up the code a bit and added some comments.
+
